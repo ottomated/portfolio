253cbefd0c5142ef7afb27dca92e663947e5ce4b
Ottomated <otto@ottomated.net>
2019-03-25 10:12:07 -0700

    seen.json: add href option

--- js/seen.js
+++ js/seen.js
@@ -1,10 +1,18 @@
 const seenEl = document.querySelector('#lastseen');
 
 function updateSeen() {
-    fetch('seen.json').then(r => r.text()).then(r => {
+    fetch('seen.json?nocache=' + Math.random().toString().substring(2)).then(r => r.text()).then(r => {
         let json = JSON.parse(r);
-        if (json.project)
-            seenEl.innerHTML = 'I was last seen working on <span style="color:#949494;">' + json.project + '</span>.';
+        if (json.project) {
+            if (json.project_href)
+                // Please no XSS
+                seenEl.innerHTML = `I was last seen working on <a href="${json.project_href}" class="d" target="_blank"
+                style="color:#949494;">${json.project}<span class="underline" style="background-color:#949494;"></span></a>.`;
+            else
+                seenEl.innerText = 'I was last seen working on ' + json.project + '.';
+        } else {
+            seenEl.innerText = "I'm not working on anything right now."
+        }
     }).catch(console.log);
 }
 
