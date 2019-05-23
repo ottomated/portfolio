351f8a91fa937bdfb4d58bc2f9c7b42dde8c5ce3
Ottomated <otto@ottomated.net>
2019-05-22 20:39:31 -0700

    Work:

---
+++ js/project_data.js
@@ -0,0 +1,7 @@
+const ProjectData = {
+	"growth_tracker": {
+		title: "Growth Tracker",
+		github: "https://github.com/ottomated/growth_tracker",
+		description: "Growth Tracker is a <a href=\"https://discordapp.com/\">Discord</a> bot that's currently used by over 2,000 servers and 1,000,000 total people. It tracks how many people are in each server over time and can generate and display graphs of this growth. This was the first project that I made that became widely used, and I'm still maintaining it alone today."
+	}
+};
